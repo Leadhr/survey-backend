a197e201ebcfd06332baf1b66718759e693b4c81
Leadhr <mhgamble1@gmail.com>
2020-07-30 12:22:30 -0500

    added passport authentication

--- server.js
+++ server.js
@@ -1,23 +1,53 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const mongoose = require('mongoose')
+const express = require("express");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-app.use(cors())
+const passport = require("./passport/setup");
+const auth = require("./routes/auth");
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', (error) => console.log(error))
-db.on('open', () => console.log('Connected to Database'))
+const app = express();
+const PORT = 3001;
 
-app.use(express.json())
+app.use(cors());
 
-const questionsRouter = require('./routes/questions')
-app.use('/questions', questionsRouter)
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.on("open", () => console.log("Connected to Database"));
 
-const answersRouter = require('./routes/answers')
-app.use('/answers', answersRouter)
+// Bodyparser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(3001, () => console.log('Server Started'))
+// Express session
+app.use(
+  session({
+    secret: "very secret this is",
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+
+app.use("/api/auth", auth)
+
+const questionsRouter = require("./routes/questions");
+app.use("/questions", questionsRouter);
+
+const answersRouter = require("./routes/answers");
+app.use("/answers", answersRouter);
+
+app.listen(PORT, () => console.log("Server Started"));
